3da39628dcd7170d35978244b0177f222ceaec1f
Nicholas Torres <torresnick272@gmail.com>
2020-09-02 19:51:22 -0700

    Added client side code (Android)

---
+++ Client/app/src/main/java/com/example/sharedexpenseapp/BindingAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.sharedexpenseapp
+
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
+import com.google.android.material.textfield.TextInputEditText
+
+@BindingAdapter("android:text")
+internal fun setText(view: EditText, value: Float?) {
+    println("Binding adapter function fired")
+    view.text = value?.let { SpannableStringBuilder(value.toString()) } ?: SpannableStringBuilder("")
+}
+
+@InverseBindingAdapter(attribute = "android:text")
+internal fun getTextString(view: EditText): Float? {
+    println("Inverse binding adapter function fired")
+    return try {
+        view.text?.toString()?.toFloat()
+    } catch (e: NumberFormatException) {
+        0.0f // because, um, what else can we do?
+    }
+    /*val editTextString: String? = view.text?.toString()
+    return if(editTextString == null || editTextString == "") 0.0f else editTextString.toString().toFloat()*/
+}
+
+@BindingAdapter("android:textAttrChanged")
+fun setTextChangedListener(editText: TextInputEditText, listener: InverseBindingListener) {
+    editText.addTextChangedListener(object: TextWatcher {
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            listener.onChange()
+        }
+    })
+}
